7dfc5b969e4a704be850e5ee33f77b8242791aff
xpower201 <vova.shirnin2018@mail.ru>
2023-05-08 13:59:42 +0500

    School

---
+++ School/engine.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import Session, create_session
+
+engine = create_engine("postgresql+psycopg2://postgres:univer@localhost/shool")
+session = Session(bind=engine)
+
+session = create_session(bind=engine)
+
+def add_data(full_name, number_class, index_class):
+    session.execute(text(f"INSERT INTO public.student(dull_name, number_class, index_class) VALUES ('{full_name}', '{number_class}', '{index_class}')"))
+    session.commit()
+    session.close()
---
+++ School/school.py
@@ -0,0 +1,73 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QComboBox, QPushButton, QLineEdit, QWidget,QGridLayout
+from engine import add_data
+
+class School(QMainWindow):
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.setWindowTitle("Добавление Ученика")
+        self.setFixedSize(500, 200)
+    
+        self.number_class = QComboBox()
+        self.number_class.addItem("1")
+        self.number_class.addItem("2")
+        self.number_class.addItem("3")
+        self.number_class.addItem("4")
+        self.number_class.addItem("5")
+        self.number_class.addItem("6")
+        self.number_class.addItem("7")
+        self.number_class.addItem("8")
+        self.number_class.addItem("9")
+        self.number_class.addItem("10")
+        self.number_class.addItem("11")
+        
+        self.index_class = QComboBox()
+        self.index_class.addItem("А")
+        self.index_class.addItem("Б")
+        self.index_class.addItem("В")
+        self.index_class.addItem("Г")
+        self.index_class.addItem("Д")
+    
+        
+        layout = QGridLayout()
+        self.lbl_0 = QLabel("Добавить ученика")
+        self.lbl_1 = QLabel("Добавить ФИО")
+        self.full_name = QLineEdit()
+        self.lbl_3 = QLabel("Выбрать класс")
+        self.lbl_4 = QLabel("Выбрать индекс класса")
+        self.btn_add = QPushButton("Добавить")
+        self.btn_exit = QPushButton("Выход")
+        self.btn_add_index = QPushButton("Добавить индекс")
+    
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+        
+        layout.addWidget(self.lbl_1, 0,0)
+        layout.addWidget(self.lbl_3, 1,0)
+        layout.addWidget(self.lbl_4, 2,0)
+        layout.addWidget(self.full_name, 0,2)
+        layout.addWidget(self.number_class, 1,2)
+        layout.addWidget(self.index_class, 2,2)
+        layout.addWidget(self.btn_add,3,3)
+        layout.addWidget(self.btn_add_index, 3,2)
+        layout.addWidget(self.btn_exit, 3,0)
+        
+        self.btn_add.clicked.connect(self.btn_add_click)
+        self.btn_exit.clicked.connect(self.btn_exit_click)      
+        
+    def btn_add_click(self):
+        full_name = self.full_name.text()
+        number_class = self.number_class.currentText()
+        index_class = self.index_class.currentText()
+        add_data(full_name, number_class, index_class)
+                
+    def btn_exit_click(self):
+        app.exit()
+       
+app = QApplication(sys.argv)
+ex = School()
+ex.show()
+app.exec()
