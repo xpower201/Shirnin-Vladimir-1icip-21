c2a5fe839ffd124d5b4c47f798e9505d944359cf
xpower201 <126676281+xpower201@users.noreply.github.com>
2023-05-09 17:29:19 +0500

    engine.py

--- School/engine.py
+++ School/engine.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import create_session
 engine = create_engine("postgresql+psycopg2://postgres:univer@localhost/shool")
 session = create_session(bind=engine)
 
-def add_data():
-    session.execute(text(f"INSERT INTO public.student(dull_name, number_class, index_class) VALUES ('(full_name)', '(number_class)', '(index_class)')"))
+def add_data(full_name, number_class, index_class):
+    session.execute(text(f"INSERT INTO public.student(dull_name, number_class, index_class) VALUES ('{full_name}', '{number_class}', '{index_class}')"))
     session.commit()
     session.close()
