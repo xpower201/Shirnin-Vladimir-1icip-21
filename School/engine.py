99c58d0fac8d754edee218bcb2bef68f81c12f19
xpower201 <126676281+xpower201@users.noreply.github.com>
2023-05-09 17:29:54 +0500

    engine.py

--- School/engine.py
+++ School/engine.py
@@ -5,6 +5,6 @@ engine = create_engine("postgresql+psycopg2://postgres:univer@localhost/shool")
 session = create_session(bind=engine)
 
 def add_data(full_name, number_class, index_class):
-    session.execute(text(f"INSERT INTO public.student(dull_name, number_class, index_class) VALUES ('{full_name}', '{number_class}', '{index_class}')"))
+    session.execute(text(f"INSERT INTO public.student(dull_name, number_class, index_class) VALUES ('full_name', 'number_class', 'index_class')"))
     session.commit()
     session.close()
