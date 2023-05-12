0b640169e67e80a6d647e3180d7f3f6ae0d9f970
xpower201 <vova.shirnin2018@mail.ru>
2023-05-12 19:42:16 +0500

    update

---
+++ test_app/auth.py
@@ -0,0 +1,124 @@
+import sys
+from PyQt6.QtCore import QTimer, Qt
+from PyQt6.QtWidgets import QApplication, QLabel, QLineEdit, QPushButton, QVBoxLayout, QMessageBox, QDialog
+import random
+from engine import text, session
+from test import Test
+
+class CaptchaDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Капча")
+        self.label = QLabel("Введите капчу:")
+        self.textbox = QLineEdit()
+        self.button = QPushButton("Проверить")
+        self.button.clicked.connect(self.verify_captcha)
+        self.generate_captcha()
+
+        
+        self.timer_label = QLabel("Таймер: 10")
+        self.timer_counter = 10
+        self.timer = QTimer()
+        self.timer.setInterval(1000)
+        self.timer.timeout.connect(self.update_timer)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.textbox)
+        layout.addWidget(self.timer_label)
+        layout.addWidget(self.button)
+
+        self.setLayout(layout)
+
+    def verify_captcha(self):
+        captcha = self.textbox.text()
+        print("Проверка капчи:", captcha)
+
+
+        if captcha.lower() == self.label.text(): 
+            self.accept()
+        else:
+            self.textbox.setDisabled(True)  
+            self.timer_counter = 11
+            self.timer.start()
+            QMessageBox.critical(self, "Ошибка", "Неправильная капча")
+
+    def start_timer(self):
+        self.timer_counter = 10
+        self.timer.start()
+
+    def update_timer(self):
+        self.timer_counter -= 1
+        self.timer_label.setText(f"Таймер: {self.timer_counter}")
+
+        if self.timer_counter == 0:
+            self.timer.stop()
+            self.textbox.setDisabled(False)
+            self.generate_captcha()
+        self.captcha_label = QLabel(self)
+        self.captcha_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        
+
+    def generate_captcha(self):
+        captcha1 = str(random.randint(1000, 9999))
+        self.label.setText(captcha1)
+
+
+
+
+class LoginWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Окно входа")
+
+        self.label_username = QLabel("Имя пользователя:")
+        self.login_edit = QLineEdit()
+        self.label_password = QLabel("Пароль:")
+        self.password_edit = QLineEdit()
+        self.button_login = QPushButton("Войти")
+        self.button_login.clicked.connect(self.login)
+
+        self.captcha_dialog = CaptchaDialog(parent=self)
+        self.captcha_dialog.setModal(True)
+
+        self.login_attempts = 0
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.label_username)
+        layout.addWidget(self.login_edit)
+        layout.addWidget(self.label_password)
+        layout.addWidget(self.password_edit)
+        layout.addWidget(self.button_login)
+
+        self.setLayout(layout)
+
+    def login(self):
+        self.sw = Test()
+
+        sql = text("SELECT * FROM public.auth")
+        obj = session.execute(sql)
+        for row in obj:
+            for login in  row:
+                self.login = login
+            for password in row:
+                self.password = password
+            if self.login_edit.text() == self.login and self.password_edit.text() == self.password:
+                self.sw.show()
+                exe.close()
+            else:
+        
+                self.captcha_dialog.start_timer()
+                
+                if self.captcha_dialog.exec() == QDialog.DialogCode.Accepted:
+                    QMessageBox.information(self, "Успех", "Вход выполнен после капчи")
+                
+                else:
+                    QMessageBox.warning(self, "Ошибка", "Неверные данные и капча")
+                    self.login_attempts = 0
+                    self.generate_captcha()
+                
+
+app = QApplication(sys.argv)
+exe = LoginWindow()
+exe.show()
+app.exec()
--- test_app/test.py
+++ test_app/test.py
@@ -1,21 +1,61 @@
-from PyQt6.QtWidgets import QMainWindow , QVBoxLayout , QLabel , QWidget
+from PyQt6.QtWidgets import QVBoxLayout, QLabel, QCheckBox, QPushButton, QMessageBox, QDialog
 
-class Test(QMainWindow):
-
+class Test(QDialog):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle(" Социальный опрос")
+        self.answers = []
 
-        self.setWindowTitle("Угадай модель машины")
-        self.setFixedSize(1000, 1000)
-        
+        self.question1 = QLabel("Какой ваш возраст?")
+        self.answer1_1 = QCheckBox("18 ")
+        self.answer1_2 = QCheckBox("30")
+        self.answer1_3 = QCheckBox("45")
+        self.answer1_4 = QCheckBox("50")
+
+        self.question2 = QLabel("Какой ваш пол?")
+        self.answer2_1 = QCheckBox("Мужской")
+        self.answer2_2 = QCheckBox("Женский")
+        self.answer2_3 = QCheckBox("Другой")
+
+        self.submit_button = QPushButton("Отправить")
+        self.submit_button.clicked.connect(self.submit_survey)
+
         layout = QVBoxLayout()
-        lbl_4 = QLabel()
+        layout.addWidget(self.question1)
+        layout.addWidget(self.answer1_1)
+        layout.addWidget(self.answer1_2)
+        layout.addWidget(self.answer1_3)
+        layout.addWidget(self.answer1_4)
+        layout.addWidget(self.question2)
+        layout.addWidget(self.answer2_1)
+        layout.addWidget(self.answer2_2)
+        layout.addWidget(self.answer2_3)
+        layout.addWidget(self.submit_button)
 
-        layout.addWidget(lbl_4)
+        self.setLayout(layout)
 
-        widget = QWidget()
-        widget.setLayout(layout)
-        self.setCentralWidget(widget)
+    def submit_survey(self):
+        age = []
+        if self.answer1_1.isChecked():
+            age.append("18")
+        if self.answer1_2.isChecked():
+            age.append("30")
+        if self.answer1_3.isChecked():
+            age.append("45")
+        if self.answer1_4.isChecked():
+            age.append("50")
 
+        gender = []
+        if self.answer2_1.isChecked():
+            gender.append("Мужской")
+        if self.answer2_2.isChecked():
+            gender.append("Женский")
+        if self.answer2_3.isChecked():
+            gender.append("Другой")
 
-   
+        if not age:
+            age = "Не выбрано"
+        if not gender:
+            gender = "Не выбрано"
+
+        QMessageBox.information(self, "Результаты опроса", f"Ваш возраст: {', '.join(age)}\nВаш пол: {', '.join(gender)}")
